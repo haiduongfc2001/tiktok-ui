b4f2ee09d1d66cc21b9819ab6d05046ead4c3f20
haiduongfc2001 <haiduongfc2001@gmail.com>
2023-08-22 15:02:34 +0700

    heeader - search bar

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,34 @@
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://p16-sign-useast2a.tiktokcdn.com/tos-useast2a-avt-0068-giso/f087b832acf74c4616195a4c97c317da~c5_100x100.jpeg?x-expires=1692860400&x-signature=C3uReWaLvve0FlUuaKXlupb6A1Y%3D"
+                alt="Hoaa"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span className={cx('avatar')}>
+                        Nguyen Van A
+                    </span>
+                    <FontAwesomeIcon
+                        className={cx('check')}
+                        icon={faCheckCircle}
+                    />
+                </h4>
+                <span className={cx('username')}>
+                    nguyenvana
+                </span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
