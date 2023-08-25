3c8b0bd9a7e7864af66c727e7139514db354901f
haiduongfc2001 <haiduongfc2001@gmail.com>
2023-08-25 14:19:36 +0700

    update button language 's icon in Menu

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,5 +1,6 @@
 const images = {
     logo: require('~/assets/images/logo.svg').default,
+    languages: require('~/assets/images/languages.svg').default,
     noImage: require('~/assets/images/no-image.png'),
 };
 
