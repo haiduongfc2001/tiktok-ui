8e00f26e9a3e3e97e5c9819b49e9be8354219647
haiduongfc2001 <haiduongfc2001@gmail.com>
2023-08-26 23:33:51 +0700

    css module component

--- src/components/FollowingAccounts/AccountItem/AccountItem.js
+++ src/components/FollowingAccounts/AccountItem/AccountItem.js
@@ -2,7 +2,7 @@ import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import Image from '~/components/Image';
-import styles from './AccountItem.module.scss';
+import styles from '~/components/SuggestedAccounts/SuggestedAccounts.module.scss';
 
 const cx = classNames.bind(styles);
 
