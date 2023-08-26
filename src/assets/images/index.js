5a19a2aec33437fd4d41ceb80a994fec67adf914
haiduongfc2001 <haiduongfc2001@gmail.com>
2023-08-26 23:33:15 +0700

    add music svg

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -2,6 +2,7 @@ const images = {
     logo: require('~/assets/images/logo.svg').default,
     languages: require('~/assets/images/languages.svg').default,
     dot: require('~/assets/images/following-dot.svg').default,
+    music: require('~/assets/images/video-music.svg').default,
     noImage: require('~/assets/images/no-image.png'),
 };
 
