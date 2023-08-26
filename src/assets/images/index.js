9d595daf64c50b436c7477af1550139ff0bc59a3
haiduongfc2001 <haiduongfc2001@gmail.com>
2023-08-26 12:19:05 +0700

    add 'dot' in MenuItem Following

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,6 +1,7 @@
 const images = {
     logo: require('~/assets/images/logo.svg').default,
     languages: require('~/assets/images/languages.svg').default,
+    dot: require('~/assets/images/following-dot.svg').default,
     noImage: require('~/assets/images/no-image.png'),
 };
 
--- src/layouts/components/Sidebar/Menu/MenuItem.js
+++ src/layouts/components/Sidebar/Menu/MenuItem.js
@@ -2,10 +2,13 @@ import classNames from 'classnames/bind';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 import styles from './Menu.module.scss';
+import images from '~/assets/images';
 
 const cx = classNames.bind(styles);
 
 function MenuItem({ title, to, icon, activeIcon }) {
+    const isFollowingTitle = title === 'Following';
+
     return (
         <NavLink
             className={(nav) => cx('menu-item', { active: nav.isActive })}
@@ -14,6 +17,13 @@ function MenuItem({ title, to, icon, activeIcon }) {
             <span className={cx('icon')}>{icon}</span>
             <span className={cx('active-icon')}>{activeIcon}</span>
             <span className={cx('title')}>{title}</span>
+            {isFollowingTitle && (
+                <img
+                    src={images.dot}
+                    className={cx('following-dot')}
+                    alt="following-dot"
+                />
+            )}
         </NavLink>
     );
 }
