c77c14ff3909d28d10169d92d011399bc76d578c
haiduongfc2001 <haiduongfc2001@gmail.com>
2023-08-27 18:47:03 +0700

    create icon video

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -2,7 +2,31 @@ const images = {
     logo: require('~/assets/images/logo.svg').default,
     languages: require('~/assets/images/languages.svg').default,
     dot: require('~/assets/images/following-dot.svg').default,
-    music: require('~/assets/images/video-music.svg').default,
+
+    like: require('~/assets/images/video-like.svg').default,
+    comment: require('~/assets/images/video-comment.svg').default,
+    favorite: require('~/assets/images/video-favorite.svg').default,
+    share: require('~/assets/images/video-share.svg').default,
+
+    embed: require('~/assets/images/shareVideo/embed.svg').default,
+    sendFriend: require('~/assets/images/shareVideo/sendFriend.svg').default,
+    shareFacebook: require('~/assets/images/shareVideo/shareFacebook.svg')
+        .default,
+    shareWhatsApp: require('~/assets/images/shareVideo/shareWhapsApp.svg')
+        .default,
+    copyLink: require('~/assets/images/shareVideo/copyLink.svg').default,
+    shareTwitter: require('~/assets/images/shareVideo/shareTwitter.svg')
+        .default,
+    shareLinkedIn: require('~/assets/images/shareVideo/shareLinkedIn.svg')
+        .default,
+    shareReddit: require('~/assets/images/shareVideo/shareReddit.svg').default,
+    shareTelegram: require('~/assets/images/shareVideo/shareTelegram.svg')
+        .default,
+    shareEmail: require('~/assets/images/shareVideo/shareEmail.svg').default,
+    shareLine: require('~/assets/images/shareVideo/shareLine.svg').default,
+    sharePinterest: require('~/assets/images/shareVideo/sharePinterest.svg')
+        .default,
+
     noImage: require('~/assets/images/no-image.png'),
 };
 
