37eede7155fd2cc341d56eef4b12882012bfcf28
haiduongfc2001 <haiduongfc2001@gmail.com>
2023-08-27 18:48:41 +0700

    create an array of video icons

---
+++ src/components/VideoItem/shareVideo.js
@@ -0,0 +1,64 @@
+import images from '~/assets/images';
+
+export const shareVideo = [
+    {
+        icon: <img src={images.embed} alt="embed" />,
+        title: 'Embed',
+        // to: '/share-video/',
+    },
+    {
+        icon: <img src={images.sendFriend} alt="Send to friends" />,
+        title: 'Send to friends',
+        // to: '/share-video/',
+    },
+    {
+        icon: <img src={images.shareFacebook} alt="Share to Facebook" />,
+        title: 'Share to Facebook',
+        // to: '/share-video/',
+    },
+    {
+        icon: <img src={images.shareWhatsApp} alt="Share to WhatsApp" />,
+        title: 'Share to WhatsApp',
+        // to: '/share-video/',
+    },
+    {
+        icon: <img src={images.copyLink} alt="Copy link" />,
+        title: 'Copy link',
+        // to: '/share-video/',
+    },
+    {
+        icon: <img src={images.shareTwitter} alt="Share to" />,
+        title: 'Share to Twitter',
+        // to: '/share-video/',
+    },
+    {
+        icon: <img src={images.shareLinkedIn} alt="Share to LinkedIn" />,
+        title: 'Share to LinkedIn',
+        // to: '/share-video/',
+    },
+    {
+        icon: <img src={images.shareReddit} alt="Share to Reddit" />,
+        title: 'Share to Reddit',
+        // to: '/share-video/',
+    },
+    {
+        icon: <img src={images.shareTelegram} alt="Share to Telegram" />,
+        title: 'Share to Telegram',
+        // to: '/share-video/',
+    },
+    {
+        icon: <img src={images.shareEmail} alt="Share to Email" />,
+        title: 'Share to Email',
+        // to: '/share-video/',
+    },
+    {
+        icon: <img src={images.shareLine} alt="Share to Line" />,
+        title: 'Share to Line',
+        // to: '/share-video/',
+    },
+    {
+        icon: <img src={images.sharePinterest} alt="Share to Pinterest" />,
+        title: 'Share to Pinterest',
+        // to: '/share-video/',
+    },
+];
